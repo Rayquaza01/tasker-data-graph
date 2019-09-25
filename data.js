49b17afba80a860ef7dba9f91a662d879494dc51
Joe Jarvis <joe.jarvis109@gmail.com>
2019-09-24 23:46:33 -0400

    Revert "Update chart.js"
    This reverts commit cff634dfe9fc7b381b300bfa70e7d19d74412760.

--- data.js
+++ data.js
@@ -1,57 +1,34 @@
-/* globals dataInfo, chartType, colors, Chart */
-
 function main() {
-    // split data by newline
-    let data = dataInfo.split("\n");
-
-    // get end date from first line. (3 two digit numbers separated by /)
-    let endDate = data[0].match(/(?:\d{2}\/?){3}/g)[0];
-    // calculate days left by subtracting the current date from the end date.
-    let daysLeft = new Date(new Date(endDate) - Date.now()).getDate();
-
-    // get used and total data from second line
-    // first element is used data, second element is total data
-    let usage = [...data[1].match(/[\d.,]+/g)].map(item =>
-        parseFloat(item.replace(",", ""))
-    );
-    let availableData = usage[1] - usage[0];
-
-    // display items on webpage
-    document.getElementById("date").innerText = data[0] + " (" + daysLeft + " days left)";
-    document.getElementById("overage").innerText = data[8];
-    document.getElementById("usage").innerText =
-        usage[0] +
-        "MB used of " +
-        usage[1] +
-        "MB (" +
-        (usage[0] / usage[1] * 100).toPrecision(3) +
-        "%)";
-
-    // get data usage for each device
-    data = data.slice(3).filter(item => item.match(/^\d{4}/));
-    let dataset = data.map(item =>
-        parseFloat(item.match(/[\d.,]+/g)[1].replace(",", ""))
-    );
-    let labels = data.map(item => item.match(/^\d{4}/g)[0]);
-
-    // add available data to dataset
+    var data = dataInfo.split("\n");
+    var endDate = data[0].match(/\d{2}\/\d{2}\/\d{2}/g)[0]
+    var daysLeftMS = new Date(endDate) - new Date();
+    var daysLeft = new Date(daysLeftMS).getDate();
+    var usedData = parseFloat(data[1].match(/[\d.,]+/g)[0].replace(",", ""));
+    var totalData = parseFloat(data[1].match(/[\d.,]+/g)[1].replace(",", ""));
+    var availableData = totalData - usedData;
+    var labels = [];
+    var dataset = [];
+    for (var i = 3; i < data.length; i++) {
+    	if (data[i].match(/^\d{4}/)) {
+            labels.push(data[i].match(/^\d{4}/g)[0]);
+            dataset.push(parseFloat(data[i].match(/[\d.,]+/g)[1].replace(",", "")));
+        } else {
+        	break;
+        }
+    }
     labels.push("Available");
     dataset.push(availableData);
-
-    // create graph
-    let ctx = document.getElementById("datagraph");
+    var ctx = document.getElementById("datagraph");
     new Chart(ctx, {
-        type: chartType,
-        data: {
-            labels: labels,
-            datasets: [
-                {
-                    data: dataset,
-                    backgroundColor: colors.split(",")
-                }
-            ]
+    	type: chartType,
+    	data: {
+        	labels: labels,
+        	datasets: [{
+            	data: dataset,
+                backgroundColor: colors.split(",")
+            }]
         }
     });
+    document.getElementById("date").innerText = data[0] + " (" + daysLeft + " days left)";
 }
-
 document.addEventListener("DOMContentLoaded", main);
